d0fb1d8a44b2546e3d3c293d178bedb5947054e9
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-07-17 01:34:49 +0430

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,12 @@ var opening = document.querySelector('.opening')
 var openNote = document.querySelector('#openNote')
 var terminal = document.querySelector('.terminal')
 var typing = document.querySelector('.typing')
+var calculator = document.querySelector('.calculator')
 var opencalculator = document.querySelector('.open-cal')
 var closecal = document.querySelector('.close-cal')
+/*var maxcal = document.querySelector('.max-cal')
+var mincal = document.querySelector('.min-cal')*/
+
 /********** HANDLERS **********/
 function handleAdding() {
   notes.style.display = 'flex'
@@ -82,46 +86,6 @@ function handleNotes() {
 }
 function handleOpenCal() {
   calculator.style.display = 'block'
-}
-
-function handleBackfullnote() {
-  note.style.width = '70%'
-  note.style.height = '430px'
-}
-
-function handleClose() {
-  terminal.style.display = 'none'
-}
-
-function handleClosenote() {
-  note.style.display = 'none'
-}
-
-function handleFull() {
-  terminal.style.width = '90%'
-  terminal.style.height = '90%'
-}
-
-function handleFullnote() {
-  note.style.width = '90%'
-  note.style.height = '90%'
-}
-
-function handleOpenLaunching() {
-  launchpad.style.display = 'block'
-  navbar.style.display = 'none'
-  container.style.display = 'none'
-}
-
-function handleOpening() {
-  terminal.style.display = 'block'
-  container.style.display = 'flex'
-  navbar.style.display = 'flex'
-  launchpad.style.display = 'none'
-}
-
-function handleOpenNote() {
-  note.style.display = 'block'
   container.style.display = 'flex'
   navbar.style.display = 'flex'
   launchpad.style.display = 'none'
@@ -176,26 +140,3 @@ function calculate(event) {
     display.value += clickedButtonValue;
   }
 }
-
-function handleDeleting() {
-  notes.style.display = 'none'
-  typing.style.display = 'none'
-}
-
-function handleNotes() {
-  typing.style.display = 'block'
-}
-
-/********** LISTENERS **********/
-adding.addEventListener('click', handleAdding)
-backfull.addEventListener('click', handleBackfull)
-backfullnote.addEventListener('click', handleBackfullnote)
-close.addEventListener('click', handleClose)
-closenote.addEventListener('click', handleClosenote)
-deleting.addEventListener('click', handleDeleting)
-full.addEventListener('click', handleFull)
-fullnote.addEventListener('click', handleFullnote)
-launching.addEventListener('click', handleOpenLaunching)
-notes.addEventListener('click', handleNotes)
-opening.addEventListener('click', handleOpening)
-openNote.addEventListener('click', handleOpenNote)
