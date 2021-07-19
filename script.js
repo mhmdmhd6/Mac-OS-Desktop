d52c4b592c37f733e11fb9927c59bd07b59b2bfc
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-07-19 19:21:51 +0430

    new

--- script.js
+++ script.js
@@ -157,3 +157,20 @@ $(function () {
 //date and time
 var d = new Date()
 document.getElementById('date').innerHTML = d.toDateString()
+
+function startTime() {
+  var today = new Date()
+  var h = today.getHours()
+  var m = today.getMinutes()
+  var s = today.getSeconds()
+  m = checkTime(m)
+  s = checkTime(s)
+  document.getElementById('txt').innerHTML = h + ':' + m + ':' + s
+  var t = setTimeout(startTime, 1000)
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i
+  } // add zero in front of numbers < 10
+  return i
+}
