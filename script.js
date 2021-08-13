789b24097d15e1820268cd9b775cf038fc44e819
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-08-13 21:49:05 +0430

    add right click menu and adding close and other icon

--- script.js
+++ script.js
@@ -346,3 +346,27 @@ function placeCaretAtEnd(el) {
   sel.addRange(range);
 }
 
+//Right click to desktop 
+    document.onclick = hideMenu;
+		document.oncontextmenu = rightClick;
+
+		function hideMenu() {
+			document.getElementById(
+				"contextMenu").style.display = "none"
+		}
+
+		function rightClick(e) {
+			e.preventDefault();
+
+			if (document.getElementById(
+				"contextMenu").style.display == "block")
+				hideMenu();
+			else {
+				var menu = document
+					.getElementById("contextMenu")
+					
+				menu.style.display = 'block';
+				menu.style.left = e.pageX + "px";
+				menu.style.top = e.pageY + "px";
+			}
+		}
