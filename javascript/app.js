5500dd31105d0f65b973c1c6984ad3e33e8a0ecb
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-09-20 23:29:31 +0430

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,29 @@
-if ("serviceWorker" in navigator) {
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.register('../sw.js')
+//     .then(function (reg) {
+//         console.log("Service worker registered", reg);
+//     })["catch"](function (err) {
+//         console.log(err);
+//     });
+// }
+
+// does the browser support service workers?
+if ('serviceWorker' in navigator) {
+    // then register our service worker
     navigator.serviceWorker.register('../sw.js')
-    .then(function (reg) {
-        console.log("Service worker registered", reg);
-    })["catch"](function (err) {
-        console.log(err);
-    });
-}
+      .then(reg => {
+        // display a success message
+        console.log(`Service Worker Registration (Scope: ${reg.scope})`);
+      })
+      .catch(error => {
+        // display an error message
+        let msg = `Service Worker Error (${error})`;
+        console.error(msg);
+        // display a warning dialog (using Sweet Alert 2)
+        Swal.fire('', msg, 'error');
+      });
+  } else {
+    // happens when the app isn't served over a TLS connection (HTTPS)
+    // or if the browser doesn't support service workers
+    console.warn('Service Worker not available');
+  }
