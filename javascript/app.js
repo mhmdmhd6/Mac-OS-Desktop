5428d961a93f0195cadc9404b6ef020a3faa9c0e
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-09-20 21:27:09 +0430

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,9 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register('../sw.js')
-    .then(function (reg) {
-        console.log("Service worker registered", reg);
-    })["catch"](function (err) {
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', {
+        scope: '../'
+    }).then(function(reg) {
+        console.log("Service worker registered")
+    }).catch(function(err) {
         console.log(err);
     });
 }
